5f51afa36b0cd8115f678af2d1b92e7952c22c11
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-26 12:21:51 -0300

    Adding component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 
+Vue.component('fl-component', {
+  data: () => {
+    return {
+      title: 'VueJs Component'}
+    },
+  template: '<h1>{{ title }}</h1>'
+})
+
 new Vue({
   //The element with id='app' is in index.html
   el: '#app',
-  template: '<h1>Vue Component</h1>'
 })
