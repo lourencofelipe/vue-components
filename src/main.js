b0d9b3b9a9fea1387a194366d124dbc98ed04031
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-26 14:15:53 -0300

    Adding local component registration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 
-Vue.component('fl-component', {
+//Global definition
+// Vue.component('fl-component', {
+//   data: () => {
+//     return {
+//       title: 'VueJs Component',
+//       counter: 0
+//     }
+//   },
+//   methods: {
+//     increment: function() {
+//       this.counter++
+//     }
+//   },
+//   template: 
+//   `<div>
+//   <h1>{{ title }}</h1>
+//   <button @click="increment">Clicked {{ counter }} times</button>
+//   </div>`
+// })
+
+const flComponent = {
   data: () => {
     return {
       title: 'VueJs Component',
@@ -17,9 +37,16 @@ Vue.component('fl-component', {
   <h1>{{ title }}</h1>
   <button @click="increment">Clicked {{ counter }} times</button>
   </div>`
-})
+}
 
 new Vue({
   //The element with id='app' is in index.html
   el: '#app',
+  components: {
+    'fl-component': flComponent
+  }
+})
+
+new Vue({
+  el: '#app2',
 })
